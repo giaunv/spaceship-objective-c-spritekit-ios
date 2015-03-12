f9f96be9522418689cd89379a892ffdf4473d801
Giau <giaunv@gmail.com>
2015-03-13 06:09:08 +0700

    Initial commit

---
+++ spaceship/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  spaceship
+//
+//  Created by lavalamp on 3/13/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
