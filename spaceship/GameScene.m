c6d85fe22b54fc1830c5f9b3ad6dc4d06dd3c3db
Giau <giaunv@gmail.com>
2015-03-13 07:45:15 +0700

    Scrolling Background

--- spaceship/GameScene.m
+++ spaceship/GameScene.m
@@ -10,16 +10,27 @@
 
 static const uint32_t shipCategory = 0x1 << 0;
 static const uint32_t obstacleCategory = 0x1 << 1;
+static const float BG_VELOCITY = 100.0;
+static inline CGPoint CGPointAdd(const CGPoint a, const CGPoint b){
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+static inline CGPoint CGPointMultiplyScalar(const CGPoint a, const CGFloat b){
+    return CGPointMake(a.x * b, a.y * b);
+}
 
 @implementation GameScene{
     SKSpriteNode *ship;
     SKAction *actionMoveUp;
     SKAction *actionMoveDown;
+    NSTimeInterval _lastUpdateTime;
+    NSTimeInterval _dt;
+    NSTimeInterval _lastMissileAdded;
 }
 
 -(id)initWithSize:(CGSize)size{
     if (self = [super initWithSize:size]){
         self.backgroundColor = [SKColor whiteColor];
+        [self initializingScrollingBackground];
         [self addShip];
         
         // Making self delegate of physics world
@@ -64,5 +75,41 @@ static const uint32_t obstacleCategory = 0x1 << 1;
             [ship runAction:actionMoveDown];
         }
     }
+}
+
+-(void)initializingScrollingBackground{
+    for (int i = 0; i < 2; i++) {
+        SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"bg"];
+        bg.position = CGPointMake(i * bg.size.width, 0);
+        bg.anchorPoint = CGPointZero;
+        bg.name = @"bg";
+        [self addChild:bg];
+    }
+}
+
+-(void)moveBg{
+    [self enumerateChildNodesWithName:@"bg" usingBlock:^(SKNode *node, BOOL *stop){
+        SKSpriteNode *bg = (SKSpriteNode *)node;
+        CGPoint bgVelocity = CGPointMake(-BG_VELOCITY, 0);
+        CGPoint amtToMove = CGPointMultiplyScalar(bgVelocity, _dt);
+        bg.position = CGPointAdd(bg.position, amtToMove);
+        
+        // Check if bg node is completely scrolled of the screen, if yes then put it at the end of the other node
+        if (bg.position.x <= -bg.size.width) {
+            bg.position = CGPointMake(bg.position.x + bg.size.width * 2, bg.position.y);
+        }
+    }];
+}
+
+-(void)update:(NSTimeInterval)currentTime{
+    if (_lastUpdateTime) {
+        _dt = currentTime - _lastUpdateTime;
+    } else {
+        _dt = 0;
+    }
+    
+    _lastUpdateTime = currentTime;
+    
+    [self moveBg];
 }
 @end
